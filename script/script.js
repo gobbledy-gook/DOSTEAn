6d9c3eb253108025609339a984f321906729618b
Bhavya Hirani <bhavya25hirani@gmail.com>
2023-01-18 11:30:32 -0600

    default menu display

--- script/script.js
+++ script/script.js
@@ -1,14 +1,11 @@
 let btn_mocktails = document.getElementById('mocktails')
 let mocktail_div = document.getElementById('Mocktails')
 
-let buffer = 'test';
+let buffer = 'Beverages_and_Deserts';
 let i=0;
 
 function reveal(param){
-    if(i>0)
-    {
-        document.getElementById(buffer).style.display = 'none';
-    }
+    document.getElementById(buffer).style.display = 'none';
     document.getElementById(param).style.display = 'inline-block';
     buffer = param;
     i=i+1;
